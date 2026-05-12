0bb5dee6a826f08ac66f3307a0d6ad0cb9cf7ee0
admin <admin@email.com>
2026-05-12 09:21:31 +0200

    test

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -4,8 +4,8 @@ from src.models import Parameters
 
 def test_sum_of_total_due_pln():
     manager = Manager(Parameters())
-    costs = manager.get_apartment_costs('apartment-1', 2024, 1)
-    settelment = manager.get_settlement('apartment-1', 2024, 1)
+    # costs = manager.get_apartment_costs('apartment-1', 2024, 1)
+    settelment = manager.get_settlement('apart-polanka', 2025, 1)
     total = 0
 
     tsettelment = manager.create_tenants_settlements(settelment)
